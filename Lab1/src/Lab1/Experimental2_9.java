0279baec4b469e92feca69c3861e7c3228d59a43
Stake18 <swjh2000@foxmail.com>
2020-10-11 00:41:33 +0800

    修正文件名

--- Lab1/src/Lab1/Experimental2_9.java
+++ Lab1/src/Lab1/Experimental2_9.java
@@ -2,7 +2,7 @@ package Lab1;
 
 import java.util.Scanner;
 
-public class Experimental2_8 {
+public class Experimental2_9 {
 	public static void main(String[] args) 
 	{
 		Scanner scan = new Scanner(System.in);
